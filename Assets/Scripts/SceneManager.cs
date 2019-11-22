9f11fbbfee805961d065cc2690859c74b624f9da
changhyeon743 <changhyeon743@gmail.com>
2019-11-22 14:10:55 +0900

    commit

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -1,12 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 public class SceneManager : MonoBehaviour {
 
     public static SceneManager instance;
 
     public List<Branch> branches = new List<Branch>();
     public List<Event> events = new List<Event>();
+
+    public Text mainText;
+    public GameObject content;
+    public Text titleText;
+
+    public GameObject destinText;
+    public GameObject canvas;
+
+
+    private List<Destination> destinList;
+
+    int currentBranchId = 0;
+  
 
     public Branch getBranch(int id)
     {
@@ -42,7 +56,7 @@ public class SceneManager : MonoBehaviour {
         events.Add(new Event("으악", "적이 죽었다.",0));
         events.Add(new Event("으악", "내가 죽었다.",1));
 
-        branches.Add(new Branch("조웅이 칼을 들었다.","멋있엉",new Destination[] {
+        branches.Add(new Branch("조웅이 칼을 들었다.", "멋있엉멋있엉멋있엉멋있엉멋있엉멋엉멋있엉멋있엉멋있엉멋있엉멋있엉멋있엉멋있엉", new Destination[] {
             new Destination("휘두른다.",0,4),
             new Destination("도망간다.",1,3),
         },2));
@@ -55,5 +69,36 @@ public class SceneManager : MonoBehaviour {
        // Debug.Log("1213: "+branches[0].destination[0].content);
 
         branches[0].printInfo();
+    }
+
+    void Start()
+    {
+        SetUI();
+    }
+
+    void SetUI()
+    {
+        mainText.text = branches[currentBranchId].content;
+        titleText.text = branches[currentBranchId].title;
+
+        destinList = branches[currentBranchId].destination;
+
+        int height = 350 / destinList.Count;
+
+        foreach (Destination i in destinList)
+        {
+            GameObject b = Instantiate(destinText);
+            b.transform.parent = canvas.transform;
+            b.GetComponent<RectTransform>().anchoredPosition.x = -500 - height;
+        }
+        
+
+    }
+
+    void SetUI(int id)
+    {
+        mainText.text = branches[id].content;
+        titleText.text = branches[id].title;
+        
     }
 }
